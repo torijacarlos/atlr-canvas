d474aa0fa2e51feb0439a9524e1e6dcae3e15e94
torijacarlos <alejandro.torija.b@gmail.com>
2025-04-22 19:52:49 -0600

    setup: sdl window

--- main.c
+++ main.c
@@ -9,8 +9,41 @@
 #include "./vendor/atlr/atlr.h"
 
 
+#define CANVAS_DEFAULT_WIDTH 500
+#define CANVAS_DEFAULT_HEIGHT 300
 
 int main() {
-    printf(":)\n");
+    
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window* window = SDL_CreateWindow("atlr - canvas", CANVAS_DEFAULT_WIDTH, CANVAS_DEFAULT_HEIGHT, SDL_WINDOW_RESIZABLE);
+    SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+    SDL_Renderer* renderer = SDL_CreateSoftwareRenderer(window_surface);
+
+    b32 running = 1;
+    SDL_Event e;
+    while (running) {
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+                case SDL_EVENT_QUIT: {
+                    running = 0;
+                } break;
+                case SDL_EVENT_KEY_UP: {
+                    if (e.key.key == 'q') {
+                        running = 0;
+                    }
+                } break;
+                default: break;
+            }
+        }
+        
+        SDL_UpdateWindowSurface(window);
+        SDL_RenderClear(renderer);
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     return 0;
 }
